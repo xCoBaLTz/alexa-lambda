7fe0e95eb9872a588a4570aace3b3cab585a12d2
Seanan Chabra <seananchabra@hotmail.com>
2019-05-09 15:40:24 -0400

    commented post request till backend is ready

--- index.js
+++ index.js
@@ -83,10 +83,11 @@ const handlers = {
         this.emit(':ask', speechOutput, speechOutput);
     },
     'CreateWorkOrderNowIntent': function() {
-        var res = request('POST', BASE_URL + '/work-orders', {
-            json: workOrderRequest,
-        });
-        var id = JSON.parse(res.getBody('utf-8').id);
+        // var res = request('POST', BASE_URL + '/work-orders', {
+        //     json: workOrderRequest,
+        // });
+        // var id = JSON.parse(res.getBody('utf-8').id);
+        var id = Math.random() * 100;
         speechOutput = 'Work order ' + id + ' was created. Do you want to create another work order?';
         this.emit(':ask', speechOutput, speechOutput);
     },
@@ -94,10 +95,11 @@ const handlers = {
         name = this.event.request.intent.slots.name.value;
         workOrderRequest.assignedTo = {};
         workOrderRequest.assignedTo.name = name;
-        var res = request('POST', BASE_URL + '/work-orders', {
-            json: workOrderRequest,
-        });
-        workOrderId = JSON.parse(res.getBody('utf-8').id);
+        // var res = request('POST', BASE_URL + '/work-orders', {
+        //     json: workOrderRequest,
+        // });
+        // workOrderId = JSON.parse(res.getBody('utf-8').id);
+        workOrderId = Math.random() * 100;
         speechOutput = 'Work order ' + workOrderId.toString() + ' was created and assigned to ' + name + '. Is there anything else I can help you with?';
         this.emit(':ask', speechOutput, speechOutput);
     },
